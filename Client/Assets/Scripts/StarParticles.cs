69a378c0f888a0b84f967728496520fdfddad6b9
Andrew Eiche <eiche_andrew@bah.com>
2014-11-11 09:41:26 -0800

    Adding some basic test controls

---
+++ Client/Assets/Scripts/StarParticles.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarParticles : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		particleSystem.Pause();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
