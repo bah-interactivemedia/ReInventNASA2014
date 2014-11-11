0dcfb43f3902e122758c54bc58f66f01010792be
Andrew Eiche <eiche_andrew@bah.com>
2014-11-11 11:17:32 -0800

    Adding crosshairs

--- Client/Assets/Scripts/InputController.cs
+++ Client/Assets/Scripts/InputController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using InControl;
 
 public class InputController : MonoBehaviour {
+	public Transform leftEye;
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +12,18 @@ public class InputController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		var inputDevice = InputManager.ActiveDevice;
+		if (inputDevice.AnyButton){
+			Debug.Log("BUTTON!!!");
+		}
+		if (inputDevice.RightTrigger.WasPressed){
+			Debug.Log("Trigger Pressed!");
+
+			Ray ray = leftEye.camera.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit) && hit.transform.tag == "Image"){
+				print("I'm targeting at " + hit.transform.name);
+			}
+		}
 	}
 }
