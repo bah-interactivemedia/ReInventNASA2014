61f2ea91fff7537c5441cbe6f22ecc7e792ed0a9
Andrew Eiche <eiche_andrew@bah.com>
2014-11-11 15:08:04 -0800

    Radial menu input

---
+++ Client/Assets/Scripts/NASAMenuItem.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NASAMenuItem : MonoBehaviour {
+	public enum RadialDirection{
+		up,
+		right,
+		left,
+		down,
+		none
+	}
+
+	public RadialDirection direction;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
