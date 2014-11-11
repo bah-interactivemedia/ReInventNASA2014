9d5745d05d2ddf0514a6c7d0317b1ce0c9f1e19f
Andrew Eiche <eiche_andrew@bah.com>
2014-11-11 10:41:03 -0800

    Oculus code

---
+++ Client/Assets/Scripts/InteractionTarget.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class InteractionTarget : MonoBehaviour {
+
+	public Transform player;
+
+	// Use this for initialization
+	void Start () {
+		LookAtPlayer();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	// Might have to delete this for levi's code later
+	void LookAtPlayer(){
+		this.transform.LookAt(player);
+	}
+}
